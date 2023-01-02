875213aab6da26f60cfe1badb66bdd2bdb380434
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2023-01-01 21:22:30 -0300

    atualização

--- src/components/Layout/Navbar/NavBar.js
+++ src/components/Layout/Navbar/NavBar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styles from '../../../styles/components/Layout/Navbar.module.css'
 import { Logo } from './Logo'
-import { LogoMobile } from './LogoMobile'
 import { MobileNavigation } from './MobileNavigation'
 import { Navigation } from './Navegation'
 
@@ -9,7 +8,6 @@ export const NavBar = () => {
   return (
     <div className={styles.navbar}>
       <div className={styles.logo}>
-        <LogoMobile />
         <Logo />
       </div>
       <div className={styles.nav}>
