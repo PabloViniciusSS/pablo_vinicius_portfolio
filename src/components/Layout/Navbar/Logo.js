eb55a034e133a29a02aea7af7f602f434689eae9
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2023-01-01 21:21:12 -0300

    atualização laytou navbar

--- src/components/Layout/Navbar/Logo.js
+++ src/components/Layout/Navbar/Logo.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import Nome from '../../../public/nome.png'
 import styles from '../../../styles/components/Layout/Navbar.module.css'
 
 export const Logo = () => {
   return (
-    <Image src={Nome} className={styles.nome}/>
+    <Link href="/" >    <Image src={Nome} className={styles.nome}/>
+    </Link>
   )
 }
--- src/components/Layout/Navbar/LogoMobile.js
+++ src/components/Layout/Navbar/LogoMobile.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import Icone from '../../../public/icone.png'
 import styles from '../../../styles/components/Layout/Navbar.module.css'
 
 export const LogoMobile = () => {
   return (
-    <Image src={Icone} className={styles.logomobile}/>
+    <Link href="/" ><Image src={Icone} className={styles.logomobile}/></Link>
   )
 }
