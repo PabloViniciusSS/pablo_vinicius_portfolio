476a87b99ba03a72c6da4e172eac7401f52ea6ec
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2023-01-03 10:36:32 -0300

    verificação de erro

--- src/components/Object/CardSobre.js
+++ src/components/Object/CardSobre.js
@@ -7,13 +7,12 @@ import { MdEmail  } from "react-icons/md";
 export const CardSobre = () => {
   return (
     <div className={styles.container_sobre}>
-    <div className={styles.contatos}>
-      <div className={styles.container_text}>
-        Tenho 27 anos, comecei meus estudos em programação no ano de 2013,
-        no curso técnico em informática para internet no ITEGO de Anápolis,  no ano de 2019 tive a oportunidade de colocar em pratica o que aprendi no curso, quando comecei em um estágio como desenvolvedor web, fiquei 2 anos na prefeitura de Anápolis e atualmente trabalho com suporte e busco voltar para área de programação.
-      </div>
+     <div className={styles.contatos}>
+        <div className={styles.container_text}>
+          Tenho 27 anos, comecei meus estudos em programação no ano de 2013,
+          no curso técnico em informática para internet no ITEGO de Anápolis,  no ano de 2019 tive a oportunidade de colocar em pratica o que aprendi no curso, quando comecei em um estágio como desenvolvedor web, fiquei 2 anos na prefeitura de Anápolis e atualmente trabalho com suporte e busco voltar para área de programação.
+        </div>
             <div className={styles.photo}></div>
-            
             </div>
     </div>
   )
