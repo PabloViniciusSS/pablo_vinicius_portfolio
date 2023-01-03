9ccd73b46a09ae1aba703bd651a70c8de7a87b80
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2023-01-03 12:54:40 -0300

    atualização navbar

--- src/components/Layout/Navbar/NavLinks.js
+++ src/components/Layout/Navbar/NavLinks.js
@@ -6,6 +6,8 @@ export const NavLinks = () => {
   return (
     <div className={styles.navbar_links}>
       <ul>
+      
+
       <Link href="#SkillSection" className={styles.botao}> Skill</Link>
       <Link href="#SobreSection"className={styles.botao}> Sobre</Link>
       <Link href="#PortfolioSection" className={styles.botao}> Portfolio</Link>
