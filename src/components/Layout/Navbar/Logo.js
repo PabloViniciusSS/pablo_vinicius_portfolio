470c64a20597027a395b7c7354d36ae635e8e8ca
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2023-01-08 18:24:37 -0300

    atualização

--- src/components/Layout/Navbar/Logo.js
+++ src/components/Layout/Navbar/Logo.js
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 export const Logo = () => {
   return (
-    <Link href="/" >    <img src='/Nome.png' className={styles.logo}/>
+    <Link href="/" >    <span className={styles.logo}> Pablo Vinícius</span>
     </Link>
   )
 }
